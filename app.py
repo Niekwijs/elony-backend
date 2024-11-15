9300b575f1f22304f27fb20cfed1f6a3cc8d59d9
Niek Wijsman <NiekWijsman@acknowledge.nl>
2024-11-15 14:44:35 +0100

    data query works

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def jacobinestrial():
 @app.route("/tabel_tesla_beursdata/<start_date>/<end_date>", methods=["get"])
 def get_tesla_bearsdata_date_range(start_date, end_date):
         df_tesla_stock = csv_loader.get_tesla_stock_range(start_date, end_date)
-        json_res = df_tesla_stock.to_json(orient = "records")
-        parsed_data = loads(json_res)
+        json_res = df_tesla_stock.to_json(orient = "columns")
+        parsed_data = loads(json_res)   
 
+        print(f'[+] This is bieng send to the frontend: {parsed_data}')
         return parsed_data
