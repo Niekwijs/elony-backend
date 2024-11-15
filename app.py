a71d711e740a43b2977dc58df7b944b8e5d42d8e
Niek Wijsman <NiekWijsman@acknowledge.nl>
2024-11-15 12:12:25 +0100

    data loader

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
+# other files/ imports
 from flask import Flask
 from flask_cors import CORS
-import beursdata_tabel
 from flask import Flask, jsonify
+from json import loads
+
+# our files/ imports
+import beursdata_tabel
+from data_loader import Loader
+
 
 app = Flask(__name__)
 CORS(app)
+csv_loader = Loader()
 
 
 @app.route("/tabel_tesla_beursdata")
 def jacobinestrial():
     parsed_beursdata_tesla = beursdata_tabel.create_tabel()
     return jsonify({"parsed": parsed_beursdata_tesla})
+
+
+@app.route("/tabel_tesla_beursdata/<start_date>/<end_date>", methods=["get"])
+def get_tesla_bearsdata_date_range(start_date, end_date):
+        df_tesla_stock = csv_loader.get_tesla_stock_range(start_date, end_date)
+        json_res = df_tesla_stock.to_json(orient = "records")
+        parsed_data = loads(json_res)
+
+        return parsed_data
