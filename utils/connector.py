5dde2d466290c7513a19b815f99ff6f66fd056e2
Niek Wijsman <NiekWijsman@acknowledge.nl>
2024-11-19 11:14:54 +0100

    connector class

--- utils/connector.py
+++ utils/connector.py
@@ -1,6 +1,35 @@
 import pyodbc
+from typing import type
 
-class Connection:
+class DbConnector:
     
+    connection: pyodbc.Connection = None
 
-    def __init__()
+    def __init__(self):
+        self.connect()
+
+
+
+    def connect(self)-> None :
+        if self.connection and not self.connection.closed:
+            print("Connection already active.")
+            return
+
+        connection_string = (
+        "DRIVER={SQL Server};"        # Driver
+        "SERVER=datadbserverdamen.database.windows.net;"         # Naam of IP van de server
+        "DATABASE=sebasindebocht;"     # Naam van de database
+        "UID=admindamen;"               # Gebruikersnaam
+        "PWD=uiop7890UIOP&*();"               # Wachtwoord
+        )
+        try:
+            self.connection = pyodbc.connect(connection_string)
+            print("Verbinding succesvol!")
+        except pyodbc.Error as e:
+            print("Fout bij verbinden:", e)
+            raise
+
+    def close(self)-> None:
+        if self.connection:
+            self.connection.close()
+            print("Connection closed.")
