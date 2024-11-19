8b399530ab6c660706ed95ac331913d2f598b3ae
Kate Ergo <KateErgo@acknowledge.nl>
2024-11-19 17:10:25 +0100

    fix app.py

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 # other files/ imports
-from flask import Flask
 from flask_cors import CORS
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_file
 from json import loads
 
 # our files/ imports
 import beursdata_tabel
 from data_loader import Loader
 from tweet_details import get_tweet_details
-from beursdata_lijngrafiek import create_grafiek
-
-#DBD-006
+from utils.connector import DbConnector
+from repositories.tsla_repo import TslaRepo
 from tweets import init_twitter_route
-
-#init_twitter_route(app)
+from beursdata_lijngrafiek import create_grafiek_matplotlib
 
 app = Flask(__name__)
 CORS(app)
 csv_loader = Loader()
+init_twitter_route(app)
+
+# db connection
+db_con: DbConnector = DbConnector()
+
+# repositories
+tsla_repo: TslaRepo = TslaRepo(db_con)
 
 
 @app.route("/tabel_tesla_beursdata")
@@ -26,16 +30,23 @@ def tesla_tabel():
     
     return jsonify({"parsed": parsed_beursdata_tesla})
 
+@app.route("/tsla/get_all")
+def get_all_tsla():
+    res = tsla_repo.get_all()
 
-@app.route("/tabel_tesla_beursdata/<start_date>/<end_date>", methods=["get"])
-def get_tesla_bearsdata_date_range(start_date, end_date):
-        df_tesla_stock = csv_loader.get_tesla_stock_range(start_date, end_date)
-        json_res = df_tesla_stock.to_json(orient = "columns")
-        parsed_data = loads(json_res)   
-        
-        print(f'[+] This is bieng send to the frontend: {parsed_data}')
-        return parsed_data
+    return jsonify({"res": res})
 
+@app.route("/tsla/get_by_date_range")
+def get_tsla_by_date_range():
+    start_date = request.args["start_date"]
+    end_date = request.args["end_date"]
+    print(start_date)
+    print(end_date)
+
+    res = tsla_repo.get_by_date_range(start_date, end_date)
+    print(res)
+    return jsonify({"res": res})
+     
 
 @app.route('/tweet/<tweet_id>', methods=['GET'])
 def tweet_details(tweet_id):
@@ -43,8 +54,13 @@ def tweet_details(tweet_id):
 
     return jsonify(tweet_data)
 
-@app.route("/lijngrafiek_beursdata")
-def tesla_beursdata_lijngrafiek():
-    parsed_beursdata_tesla = create_grafiek()
-    return jsonify({"parsed": parsed_beursdata_tesla})
 
+# @app.route("/lijngrafiek_beursdata")
+# def tesla_beursdata_lijngrafiek():
+#     parsed_beursdata_tesla = create_grafiek()
+#     return jsonify({"parsed": parsed_beursdata_tesla})
+
+@app.route("/lijngrafiek_beursdata_matplotlib", methods=["GET"])
+def tesla_beursdata_lijngrafiek_matplotlib():
+    img_buffer = create_grafiek_matplotlib()
+    return send_file(img_buffer, mimetype='image/png')
