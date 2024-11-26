62cba4706c0b0f258d5e23640eef7cb6308366fd
Ayyoub Assalhioui <AyyoubAssalhioui@acknowledge.nl>
2024-11-26 15:34:53 +0100

    a

--- tweets.py
+++ tweets.py
@@ -3,21 +3,31 @@ import pandas as pd
 from json import loads, dumps
 from flask import jsonify
 
+#tweets = None
+
+#def init_twitter_route(app):
+#    global tweets
+#    tweets = pd.read_csv("./data/TweetsElonMusk.csv")
+#    tweets['id'] = range(len(tweets))
+
 def init_twitter_route(app):
+    #tweets = pd.read_csv("./data/TweetsElonMusk.csv")
     @app.route("/ELONTWITTERANALYSE", methods = ['GET'])
     def get_tweets():
         try:
             #CSV-bestand inladen
-            tweets = pd.read_csv("./data/TweetsElonMusk.csv")
+            
             #Alleen relevante kolommen selecteren
-            relevant_data = tweets[["date", "time", "tweet"]]
+            relevant_data = tweets[["date", "time", "tweet", "id"]]
             #Converteer naar JSON
             tweets_json = relevant_data.to_dict(orient="records")
             return jsonify(tweets_json)
         except Exception as e:
             return jsonify({"error": str(e)}), 500
     @app.route("/ELONTWITTERDETAIL/<tweet_id>", methods=['GET'])
+
     def get_tweet_details(tweet_id):
+        print("qq")
         try:
             # Zoek de specifieke tweet op basis van ID
             tweet_details = tweets[tweets['id'] == int(tweet_id)].to_dict(orient="records")
@@ -30,3 +40,7 @@ def init_twitter_route(app):
             return jsonify(tweet_details[0])  # Retourneer de eerste match als JSON
         except Exception as e:
             return jsonify({"error": str(e)}), 500
+
+# Dataset één keer inladen en meegeven bij het initialiseren
+tweets = pd.read_csv("./data/TweetsElonMusk.csv")
+#tweets['id'] = range(len(tweets))  # Voeg een unieke ID-kolom toe
